9188de7603e8aa95af2fe1506fea3bdb7f675ba2
Masahiro Aikawa <aikawame@wor.jp>
2020-11-26 23:40:00 +0900

    公開鍵を使ってデプロイできるようにする

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,17 @@ set :application, 'smarthome-api'
 set :repo_url, 'git@github.com:aikawame/smarthome-api.git'
 set :branch, 'main'
 set :linked_files, %w[config/master.key]
+set :linked_dirs, %w[log tmp/pids]
 
+# 初回デプロイ時にエラーになる…
 set :puma_conf, "#{current_path}/config/puma/production.rb"
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.7.2'
+
+set :ssh_options, {
+  user: 'pi',
+  keys: %w[~/.ssh/id_rsa],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
